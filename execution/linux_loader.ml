cae6b2f38fb9a12b0d8fa7ac96bade97855a2280
Stephen McCamant <mccamant@cs.umn.edu>
2014-05-30 17:27:31 -0500

    Better error message for base address mismatch
    ------------------------------------------------------------------------
    r158 | jthayer | 2014-05-13 16:23:53 -0500 (Tue, 13 May 2014) | 2 lines
    
    Replaced an assert failure with an equivalent failwith.  Part of diagnosing #16.
    ------------------------------------------------------------------------

--- execution/linux_loader.ml
+++ execution/linux_loader.ml
@@ -341,7 +341,10 @@ let load_dynamic_program (fm : fragment_machine) fname load_base
       (fun phr ->
 	 if phr.ph_type = 1L then (* PT_LOAD *)
 	   (if phr.ph_flags = 5L && extra_vaddr = 0L then
-	      assert(phr.vaddr = load_base);
+               (if (phr.vaddr <> load_base) then
+                   (failwith (Printf.sprintf
+                                "Linux_loader::load_dynamic_program: phr.vaddr neq load_base\t 0x%Lx <> 0x%Lx"
+                                phr.vaddr load_base)));
 	    if data_too || (phr.ph_flags <> 6L && phr.ph_flags <> 7L) then
 	      load_segment fm ic phr extra_vaddr true)
 	 else if phr.ph_type = 3L then (* PT_INTERP *)
