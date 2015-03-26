67e767497af649abb7439d1b38186a8f540de531
Stephen McCamant <mccamant@cs.umn.edu>
2015-03-25 23:24:40 -0500

    Add "make nodebug" in place (flipped) of old "make debug"
    ------------------------------------------------------------------------
    r2103 | smccamant | 2015-03-13 13:21:19 -0500 (Fri, 13 Mar 2015) | 6 lines
    
    Swap how we control whether to have OCaml debugging information in
    FuzzBALL. The plan from r1508 of having a target named "debug" didn't
    work well because Vine has a module named "debug". Instead switch
    debug back to being the default, and make a "nodebug" target for
    CGC production use.
    ------------------------------------------------------------------------

--- config/recursive.mk
+++ config/recursive.mk
@@ -2,6 +2,11 @@
 # http://uucode.com/blog/2005/08/10/adding-recursive-targets-to-automake/
 # sojeong
 
+# There's apparently proper support for this in automake 1.13 forward,
+# but I think it's too soon to depend on that
+
+RECURSIVE_TARGETS += bytecode-recursive nodebug-recursive
+
 bytecode-recursive:
 
 ifneq ($(RECURSIVE_TARGETS),bytecode-recursive)
@@ -14,3 +19,16 @@ bytecode: all bytecode-recursive bytecode-am
 bytecode-am: bytecode-local
 
 bytecode-local:
+
+nodebug-recursive:
+
+ifneq ($(RECURSIVE_TARGETS),all-recursive nodebug-recursive)
+nodebug-recursive:
+	$(MAKE) $(AM_MAKEFLAGS) RECURSIVE_TARGETS="all-recursive nodebug-recursive" nodebug-recursive
+endif
+
+nodebug: nodebug-recursive nodebug-am
+
+nodebug-am: nodebug-local
+
+nodebug-local:
