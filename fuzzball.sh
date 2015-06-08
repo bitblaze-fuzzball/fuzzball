befba1c9005bbce98b062680f3568e8c197f6663
Stephen McCamant <mccamant@cs.umn.edu>
2015-06-08 16:19:49 -0500

    Update fuzzbal.sh convenience script
    ------------------------------------------------------------------------
    r2813 | jbenton | 2015-06-01 22:52:24 -0500 (Mon, 01 Jun 2015) | 4 lines
    
    this script only used for testing---better way to run fuzzball (been
    using it for a while, but just never got to committing it).
    ------------------------------------------------------------------------

--- fuzzball.sh
+++ fuzzball.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-schroot -c wheezy-i386 -- $CGC_HOME/code/fuzzball/exec_utils/fuzzball $DEFAULT_FUZZ_ARGS -pov-xml-output povinfo/ --always CGC_POV info povinfo/ $@
+
+export LD_LIBRARY_PATH=$CGC_HOME/code/release/bin32/:$LD_LIBRARY_PATH
+
+exec $CGC_HOME/code/fuzzball/exec_utils/fuzzball $DEFAULT_FUZZ_ARGS -pov-xml-output povinfo/ --always CGC_POV info povinfo/ $@ 
+
