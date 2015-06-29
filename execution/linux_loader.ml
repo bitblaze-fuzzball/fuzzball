95cb4c8f6f93d39d25f0e7052e46d0407e6db775
Stephen McCamant <mccamant@cs.umn.edu>
2015-06-29 00:18:05 -0500

    Generalize basic ELF loading for 64-bit
    Parse the ELF and program headers in their 64-bit formats, which is
    enough to load segments. But the process-state-setup hasn't been
    updated yet.

--- execution/linux_loader.ml
+++ execution/linux_loader.ml
@@ -7,6 +7,11 @@ open Exec_options
 open Fragment_machine
 open Linux_syscalls
 
+(* Our versions of ELF structures use int64 for any value that is
+   either always 32 bits, or 32 or 64 depending on the architecture. This
+   fits with FuzzBALL's general strategy of not bothering with int32s,
+   and also means we don't need separate 32-bit and 64-bit structure
+   versions. *)
 type elf_header = {
   eh_type : int;
   machine : int;
@@ -51,51 +56,96 @@ let read_ui32 i =
 let read_elf_header ic =
   let i = IO.input_channel ic in
   let ident = IO.really_nread i 16 in
-    assert(ident = "\x7fELF\001\001\001\000\000\000\000\000\000\000\000\000" ||
-	   ident = "\x7fELF\001\001\001\003\000\000\000\000\000\000\000\000");
-    (* OCaml structure initialization isn't guaranteed to happen
-       left to right, so we need to use a bunch of lets here: *)
-    let eh_type = IO.read_ui16 i in
-    let machine = IO.read_ui16 i in
-    let version = read_ui32 i in
-    let entry = read_ui32 i in
-    let phoff = read_ui32 i in
-    let shoff = read_ui32 i in
-    let eh_flags = read_ui32 i in
-    let ehsize = IO.read_ui16 i in
-    let phentsize = IO.read_ui16 i in
-    let phnum = IO.read_ui16 i in
-    let shentsize = IO.read_ui16 i in
-    let shnum = IO.read_ui16 i in
-    let shstrndx = IO.read_ui16 i in
-    let eh = {
-      eh_type = eh_type; machine = machine; version = version;
-      entry = entry; phoff = phoff; shoff = shoff; eh_flags = eh_flags;
-      ehsize = ehsize; phentsize = phentsize; phnum = phnum;
-      shentsize = shentsize; shnum = shnum; shstrndx = shstrndx }
-    in
-      assert(eh.ehsize = 16 + 36);
-      eh
+    match ident with
+      | ("\x7fELF\001\001\001\000\000\000\000\000\000\000\000\000"|
+	 "\x7fELF\001\001\001\003\000\000\000\000\000\000\000\000") ->
+	  (* 32-bit *)
+	  (* OCaml structure initialization isn't guaranteed to happen
+	     left to right, so we need to use a bunch of lets here: *)
+	  let eh_type = IO.read_ui16 i in
+	  let machine = IO.read_ui16 i in
+	  let version = read_ui32 i in
+	  let entry = read_ui32 i in
+	  let phoff = read_ui32 i in
+	  let shoff = read_ui32 i in
+	  let eh_flags = read_ui32 i in
+	  let ehsize = IO.read_ui16 i in
+	  let phentsize = IO.read_ui16 i in
+	  let phnum = IO.read_ui16 i in
+	  let shentsize = IO.read_ui16 i in
+	  let shnum = IO.read_ui16 i in
+	  let shstrndx = IO.read_ui16 i in
+	  let eh = {
+	    eh_type = eh_type; machine = machine; version = version;
+	    entry = entry; phoff = phoff; shoff = shoff; eh_flags = eh_flags;
+	    ehsize = ehsize; phentsize = phentsize; phnum = phnum;
+	    shentsize = shentsize; shnum = shnum; shstrndx = shstrndx }
+	  in
+	    assert(eh.ehsize = 16 + 36);
+	    eh
+      | ("\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00"|
+         "\x7fELF\x02\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00") ->
+	  (* 64-bit *)
+	  let eh_type = IO.read_ui16 i in
+	  let machine = IO.read_ui16 i in
+	  let version = read_ui32 i in
+	  let entry = IO.read_i64 i in
+	  let phoff = IO.read_i64 i in
+	  let shoff = IO.read_i64 i in
+	  let eh_flags = read_ui32 i in
+	  let ehsize = IO.read_ui16 i in
+	  let phentsize = IO.read_ui16 i in
+	  let phnum = IO.read_ui16 i in
+	  let shentsize = IO.read_ui16 i in
+	  let shnum = IO.read_ui16 i in
+	  let shstrndx = IO.read_ui16 i in
+	  let eh = {
+	    eh_type = eh_type; machine = machine; version = version;
+	    entry = entry; phoff = phoff; shoff = shoff; eh_flags = eh_flags;
+	    ehsize = ehsize; phentsize = phentsize; phnum = phnum;
+	    shentsize = shentsize; shnum = shnum; shstrndx = shstrndx }
+	  in
+	    assert(eh.ehsize = 16 + 48);
+	    eh
+      | _ ->
+	  failwith "Unrecognized identification bytes in ELF file"
 
 let read_program_headers ic eh =
-  assert(eh.phentsize = 32);
+  assert(eh.phentsize = 32 || eh.phentsize = 56);
   seek_in ic (Int64.to_int eh.phoff);
   let i = IO.input_channel ic in
     ExtList.List.init eh.phnum
-      (fun _ -> 
-	 let ph_type = read_ui32 i in
-	 let offset = read_ui32 i in
-	 let vaddr = read_ui32 i in
-	 let paddr = read_ui32 i in
-	 let filesz = read_ui32 i in
-	 let memsz = read_ui32 i in
-	 let ph_flags = read_ui32 i in
-	 let align = read_ui32 i in
-	   { ph_type = ph_type; offset = offset; vaddr = vaddr;
-	     paddr = paddr; filesz = filesz; memsz = memsz;
-	     ph_flags = ph_flags;
-	     align = align
-	   })
+      (fun _ ->
+	 match eh.phentsize with
+	   | 32 -> (* 32-bit *)
+	       let ph_type = read_ui32 i in
+	       let offset = read_ui32 i in
+	       let vaddr = read_ui32 i in
+	       let paddr = read_ui32 i in
+	       let filesz = read_ui32 i in
+	       let memsz = read_ui32 i in
+	       let ph_flags = read_ui32 i in
+	       let align = read_ui32 i in
+		 { ph_type = ph_type; offset = offset; vaddr = vaddr;
+		   paddr = paddr; filesz = filesz; memsz = memsz;
+		   ph_flags = ph_flags;
+		   align = align
+		 }
+	   | 56 -> (* 64-bit, n.b. slightly different order *)
+	       let ph_type = read_ui32 i in
+	       let ph_flags = read_ui32 i in
+	       let offset = IO.read_i64 i in
+	       let vaddr = IO.read_i64 i in
+	       let paddr = IO.read_i64 i in
+	       let filesz = IO.read_i64 i in
+	       let memsz = IO.read_i64 i in
+	       let align = IO.read_i64 i in
+		 { ph_type = ph_type; offset = offset; vaddr = vaddr;
+		   paddr = paddr; filesz = filesz; memsz = memsz;
+		   ph_flags = ph_flags;
+		   align = align
+		 }
+	   | _ -> failwith "Unsupported program header size")
 
 let store_page fm vaddr str =
   if Int64.rem vaddr 0x1000L = 0L && (String.length str) = 4096 then
@@ -316,13 +366,11 @@ let build_startup_state fm eh load_base ldso argv =
       List.iter push_word (List.rev argv_locs);
       push_word (Int64.of_int (List.length argv)); (* argc *)
       if !opt_trace_setup then
-	Printf.printf "Initial ESP is 0x%08Lx\n" !esp;
-      let sp = match !opt_arch with
-	| X86 -> R_ESP
-	| X64 -> R_RSP
-	| ARM -> R13
-      in
-	fm#set_word_var sp !esp      
+	Printf.printf "Initial stack pointer is 0x%08Lx\n" !esp;
+      match !opt_arch with
+	| X86 -> fm#set_word_var R_ESP !esp
+	| ARM -> fm#set_word_var R13 !esp
+	| X64 -> fm#set_long_var R_RSP !esp
 
 let load_dynamic_program (fm : fragment_machine) fname load_base
     data_too do_setup extras argv =
